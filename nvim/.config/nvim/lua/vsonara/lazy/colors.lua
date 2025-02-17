1c22e5ab1e01d50bfda02cd9b5b6a3d1b21e51c3
Vimal Sonara <vvsonara95@gmail.com>
2025-02-17 13:12:42 +0530

    chore(nvim): remove colorscheme change function

--- nvim/.config/nvim/lua/vsonara/lazy/colors.lua
+++ nvim/.config/nvim/lua/vsonara/lazy/colors.lua
@@ -1,25 +1,18 @@
-function ColorMyPencils(color)
-  color = color or 'tokyonight-night'
-  vim.cmd.colorscheme(color)
-
-  -- this will make background transparent
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-end
-
 return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1000,
   },
   {
-    "ellisonleao/gruvbox.nvim", name = 'gruvbox', priority = 1000, config = true,
+    "ellisonleao/gruvbox.nvim",
+    name = 'gruvbox',
+    priority = 1000,
+    config = true,
   },
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    config = function() end,
   },
   {
     'folke/tokyonight.nvim',
@@ -31,9 +24,12 @@ return {
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
 
+      -- this will make background transparent
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
-      ColorMyPencils 'tokyonight-night'
     end,
   },
 }
