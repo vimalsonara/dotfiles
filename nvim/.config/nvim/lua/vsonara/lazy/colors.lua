7eec50d91ed02ca59658e4e0adff9bf5d3b98b3f
Vimal Sonara <vvsonara95@gmail.com>
2025-03-13 16:51:36 +0530

    feat(nvim): add onedarkpro colorscheme

--- nvim/.config/nvim/lua/vsonara/lazy/colors.lua
+++ nvim/.config/nvim/lua/vsonara/lazy/colors.lua
@@ -18,16 +18,11 @@ return {
     'folke/tokyonight.nvim',
     name = 'tokyonight-night',
     priority = 1000,
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    name = 'onedark',
-    priority = 1000,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- this will make background transparent
       -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
@@ -37,5 +32,10 @@ return {
       vim.cmd.hi 'Comment gui=none'
     end,
 
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    name = 'onedark',
+    priority = 1000,
   }
 }
