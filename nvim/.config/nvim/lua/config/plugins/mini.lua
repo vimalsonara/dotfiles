697b541cd1c227472436ca650fea920f99054aa4
Vimal Sonara <vvsonara95@gmail.com>
2025-10-20 11:32:08 +0530

    chore(nvim): update mini.nvim
    - add mini.git
    - add mini.diff
    - remove mini.icons from dependency

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -1,10 +1,18 @@
 return {
 	{
 		"echasnovski/mini.nvim",
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		config = function()
+			local icons = require("mini.icons")
+			icons.setup()
+
 			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = true })
+
+			local git = require("mini.git")
+			git.setup()
+
+			local diff = require("mini.diff")
+			diff.setup()
 		end,
 	},
 }
